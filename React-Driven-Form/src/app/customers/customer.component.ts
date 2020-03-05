b77ca7cfc425be06559bd9e146fed2de858bce5b
andrew7771 <aturyansky33@gmail.com>
2020-03-05 14:46:06 +0400

    added nested formGroup

--- React-Driven-Form/src/app/customers/customer.component.ts
+++ React-Driven-Form/src/app/customers/customer.component.ts
@@ -3,6 +3,22 @@ import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean} | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+
+  return {'match': true};
+}
+
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean} | null => {
     if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
@@ -28,11 +44,14 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
       phone:'',
       notification: 'email',
       rating: [null, ratingRange(1,5)],
-      sendCatalog: {value: true, disabled: false}
+      sendCatalog: true
     });   
   }
 
