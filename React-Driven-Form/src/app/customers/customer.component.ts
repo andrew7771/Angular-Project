6f63cba321612b3b021accea7712d6645f3ee226
andrew7771 <aturyansky33@gmail.com>
2020-03-05 18:23:26 +0400

    added reacting to changes

--- React-Driven-Form/src/app/customers/customer.component.ts
+++ React-Driven-Form/src/app/customers/customer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Customer } from './customer';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
-
+import { debounceTime } from 'rxjs/operators';
 
 function emailMatcher(c: AbstractControl): { [key: string]: boolean} | null {
   const emailControl = c.get('email');
@@ -37,6 +37,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
+
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
+  }
 
   constructor(private fb: FormBuilder) { }
 
@@ -53,13 +59,33 @@ export class CustomerComponent implements OnInit {
       rating: [null, ratingRange(1,5)],
       sendCatalog: true
     });   
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    )
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
+
   }
 
   save() {
     console.log(this.customerForm);
-    console.log('Saved: ' + JSON.stringify(this.customerForm));
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));    
   }
-
+  
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');     
+    }
+  }
+  
   populateTestData() {
     // this.customerForm.setValue({
     //   firstName: 'Lack',
