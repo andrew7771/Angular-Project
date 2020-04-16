1b79744e76551edf68ecaee8f1c72f39fe896bbc
andrew7771 <aturyansky33@gmail.com>
2020-04-16 11:49:27 +0300

    moving to reactive behavior

--- APM-RxJS/src/app/products/product-list.component.ts
+++ APM-RxJS/src/app/products/product-list.component.ts
@@ -1,35 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, EMPTY } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products: Product[] = [];
+  products$: Observable<Product[]>;
   sub: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe(
-        products => this.products = products,
-        error => this.errorMessage = error
-      );
+    this.products$ = this.productService.getProducts()
+      .pipe(
+        catchError(err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      );      
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  
 
   onAdd(): void {
     console.log('Not yet implemented');
